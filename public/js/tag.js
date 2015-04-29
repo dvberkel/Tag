71bd9454b0f9679fa97cedb8cfb12cb315383e4c
dvberkel <daan.v.berkel.1980@gmail.com>
2015-04-29 15:23:38 +0200

    introduce tagged players

--- public/js/tag.js
+++ public/js/tag.js
@@ -15,7 +15,8 @@
     var Game = tag.Game = function(){
         this.updateState({
             'tagger': { 'x': 100, 'y': 100 },
-            'players': [ { 'x': 300, 'y': 300 } ]
+            'players': [ { 'x': 300, 'y': 300 } ],
+            'tagged': [ { 'x': 20, 'y': 50 }, { 'x': 80, 'y': 30 }]
         })
     };
     Game.prototype.updateState = function(state) {
@@ -24,18 +25,29 @@
 
     var GameView = tag.GameView = function(game, canvas, options){
         this.options = extend(options || {}, { 'players' : { 'fillStyle': 'blue', 'radius': 10 }}
-                              , { 'tagger': { 'fillStyle': 'orange', radius: 50 }});
+                              , { 'tagger': { 'fillStyle': 'orange', 'radius': 50 }}
+                              , { 'tagged': { 'fillStyle': 'gray', 'radius': 5 }});
         this.game = game;
         this.canvas = canvas;
         this.context = this.canvas.getContext('2d');
     }
     GameView.prototype.update = function(){
         this.clear()
+        this.paintTagged();
         this.paintTagger();
         this.paintPlayers();
     };
     GameView.prototype.clear = function(){
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    };
+    GameView.prototype.paintTagged = function(){
+        var ctx = this.context; var options = this.options.tagged;
+        ctx.fillStyle = options.fillStyle;
+        ctx.beginPath();
+        this.game.state.tagged.forEach(function(tag){
+            ctx.arc(tag.x, tag.y, options.radius, 0, 2 * Math.PI);
+        });
+        ctx.fill();
     };
     GameView.prototype.paintPlayers = function(){
         var ctx = this.context; var options = this.options.players;
