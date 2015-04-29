9922d64475f4cf3225d9cc18525b9c83d33b32ea
dvberkel <daan.v.berkel.1980@gmail.com>
2015-04-29 13:34:30 +0200

    Create a skeletal game

---
+++ public/js/application.js
@@ -0,0 +1,13 @@
+(function(tag){
+    var canvas = document.getElementById('playground');
+
+    var game = new tag.Game();
+
+    var view = new tag.GameView(game, canvas);
+
+    function animate(){
+	view.update();
+	requestAnimationFrame(animate);
+    }
+    animate();
+})(tag
