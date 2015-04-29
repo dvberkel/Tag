38cc94d108430046d626450a37f1a7ed6899cb80
dvberkel <daan.v.berkel.1980@gmail.com>
2015-04-29 11:26:13 +0200

    Create a express server

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+var express = require('express');
+var app = express();
+
+app.get('/', function(request, response){
+    response.send('Hello World');
+});
+
+var server = app.listen(3000, function(){
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Listening on http://%s:%s', host, port);
+});
