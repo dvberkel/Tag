7c20d2d818e2eb46d65f14ba564bf5f4636558af
dvberkel <daan.v.berkel.1980@gmail.com>
2015-04-29 15:06:46 +0200

    Log mouse movement when it is on the canvas

--- public/js/application.js
+++ public/js/application.js
@@ -10,4 +10,14 @@
         requestAnimationFrame(animate);
     }
     animate();
+
+    function mouseMoveHandler(event){
+        console.log(event);
+    }
+    canvas.addEventListener('mouseenter', function(){
+        canvas.addEventListener('mousemove', mouseMoveHandler);
+    });
+    canvas.addEventListener('mouseleave', function(){
+        canvas.removeEventListener('mousemove', mouseMoveHandler);
+    });
 })(tag);
