0a5455a4c40e096866ce5c61a163c61185001879
dvberkel <daan.v.berkel.1980@gmail.com>
2015-04-30 07:25:32 +0200

    Identify socket by id

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ server.listen(3000, function(){
 });
 
 io.on('connection', function(socket){
-    console.log('a user connected');
+    console.log('%s connected', socket.id);
 
     socket.on('disconnect', function(){
-        console.log('a user disconnected');
+        console.log('%s disconnected', socket.id);
     });
 });
