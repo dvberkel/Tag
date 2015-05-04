cced571617c29a871e15842ee52dc0a031e68232
dvberkel <daan.v.berkel.1980@gmail.com>
2015-05-04 07:13:39 +0200

    Client reads from options.json

--- public/js/application.js
+++ public/js/application.js
@@ -1,8 +1,19 @@
 (function(tag){
+    function getJson(url){
+        var request = new XMLHttpRequest();
+
+        request.open('GET', url, false);
+        request.send(null);
+        return JSON.parse(request.responseText);
+    }
+
     var canvas = document.getElementById('playground');
 
+    var options = getJson('options.json');
+    console.log(options);
+
     var game = new tag.Game();
-    var view = new tag.GameView(game, canvas);
+    var view = new tag.GameView(game, canvas, options);
     function animate(){
         view.update();
         requestAnimationFrame(animate);
